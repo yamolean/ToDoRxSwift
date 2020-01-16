e9595759b4c0754d4a8c15a28cf680d179f251bb
yamolean <moririnmanro9608@gmail.com>
2020-01-16 12:21:24 +0900

    podUpdate

--- TodoApp RxSwift/ViewController/AddTaskViewController.swift
+++ TodoApp RxSwift/ViewController/AddTaskViewController.swift
@@ -7,10 +7,23 @@
 //
 
 import UIKit
+import RxSwift
 
 class AddTaskViewController: UIViewController {
     
+    @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var taskTitleTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func save() {
+        guard let priority = Priority(rawValue: self.prioritySegmentedControl.selectedSegmentIndex),
+            let title = self.taskTitleTextField.text else {
+            return
+        }
+        
+        let task = Task(title: title, priority: priority)
     }
 }
